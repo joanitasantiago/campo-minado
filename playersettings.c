a845f4aa8a0d686ded6d1326c49719a21badaafe
joanitasantiago <joanitamirandasantiago@gmail.com>
2022-07-25 15:21:27 +0200

    CPMD/004.1 - refactoring

--- playersettings.c
+++ playersettings.c
@@ -8,12 +8,14 @@
 
 /* * * * * *  F U N C T I O N S  * * * * * */
 
+/* * * BOARD SETTINGS * * */
+
 /*Returns a user-entered number that refers to the chosen game difficulty*/
-int defineDifficulty()
+int PlayerDefinesGameDifficulty()
 {
     int difficulty;
-    printf("Choose the board configuration:\n<1> EASY\n<2> INTERMEDIATE\n<3> EXPERT\n");
-    printf("Type here:\t");
+    printf("Choose the board level:\n<1> EASY\n<2> INTERMEDIATE\n<3> EXPERT\n");
+    printf(">>:\t");
     scanf(" %d", &difficulty);
     while ((difficulty < 1) || (difficulty > 3))
     {
@@ -23,22 +25,73 @@ int defineDifficulty()
     return difficulty;
 }
 
-/*Open a cell according to the number entered by the user */
-void playerChooseCellToOpen(int row, int column, Cell board[row][column])
+/*Define the number of rows and columns that will form the board based on the difficulty chosen by the user*/
+void defineRowsAndColumns(int gameDifficulty, int *row, int *column)
 {
-    int selectedRow, selectedColumn;
-    printf("Type the cell you want to open:\t");
-    scanf(" %d %d", &selectedRow, &selectedColumn);
-    checkValidRowAndColumn(row, column,&selectedRow,&selectedColumn);
-    board[selectedRow][selectedColumn].isOpen = true;
+    switch (gameDifficulty)
+    {
+    case 1:
+        *row = 9;
+        *column = 9;
+        break;
+    case 2:
+        *row = 16;
+        *column = 16;
+        break;
+    case 3:
+        *row = 30;
+        *column = 16;
+        break;
+    }
 }
 
-/*Checks if the number entered is valid*/
-void checkValidRowAndColumn(int row, int column, int *selectedRow, int *selectedColumn)
+/*Set the amount of bombs on the board based on the chosen difficulty*/
+int defineAmountOfBombs(int gameDifficulty)
 {
-    while ((*selectedRow < 0) || (*selectedRow >= row) || (*selectedColumn < 0) || (*selectedColumn >= column))
+    int maxBombs;
+    switch (gameDifficulty)
+    {
+    case 1:
+        maxBombs = 10;
+        break;
+    case 2:
+        maxBombs = 40;
+        break;
+    case 3:
+        maxBombs = 99;
+        break;
+    }
+    return maxBombs;
+}
+
+
+/* * * GAMEPLAY * * */
+
+/* Asks the user to enter a row and column number */
+void playerTypesRowAndColumn(int *rowToBeOpened, int *columnToBeOpened)
+{
+    int tempRow, tempColumn;
+    printf("Type the [ROW] and [COLUMN] you want to open:\t");
+    scanf(" %d %d", &tempRow, &tempColumn);
+    *rowToBeOpened = tempRow;
+    *columnToBeOpened = tempColumn;
+}
+
+/* Checks if the number entered is valid */
+void checkValidRowAndColumn(int row, int column, int *rowToBeChecked, int *columnToBeChecked)
+{
+    int tempRow, tempColumn;
+    while ((*rowToBeChecked < 0) || (*rowToBeChecked > row) || (*columnToBeChecked < 0) || (*columnToBeChecked > column))
     {
         printf("Invalid option - try again:\t");
-        scanf(" %d %d", &selectedRow, &selectedColumn);
+        scanf(" %d %d", &tempRow, &tempColumn);
+        *rowToBeChecked = tempRow;
+        *columnToBeChecked = tempColumn;
     }
+}
+
+/*Open a cell according to the number entered by the user */
+void openCellChosenbyPlayer(int rowToBeOpened, int columnToBeOpened, int row, int column, Cell board[row][column])
+{
+    board[rowToBeOpened][columnToBeOpened].isOpen = true;
 }
